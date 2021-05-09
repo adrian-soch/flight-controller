8b2b07ce61ec26facf5f35a5f303fab8051ad237
Adrian Sochaniwsky <adr.soch@outlook.com>
2021-05-08 20:17:57 -0400

    Add support for arming ESC

---
+++ config.h
@@ -0,0 +1,21 @@
+#define ESC_PIN_0 5
+#define ESC_PIN_1 6
+#define ESC_PIN_2 12
+#define ESC_PIN_3 13
+
+#define SPEED_MIN_0 1040
+#define SPEED_MIN_1 1040
+#define SPEED_MIN_2 1040
+#define SPEED_MIN_3 1040
+
+#define SPEED_MAX_0 1960
+#define SPEED_MAX_1 1960
+#define SPEED_MAX_2 1960
+#define SPEED_MAX_3 1960
+
+#define SPEED_ARM_0 1200
+#define SPEED_ARM_1 1200
+#define SPEED_ARM_2 1200
+#define SPEED_ARM_3 1200
+
+#define SEA_LEVEL_HPA 1033.45 /* 1016.9/(1-(136/44330))^5.255 */
